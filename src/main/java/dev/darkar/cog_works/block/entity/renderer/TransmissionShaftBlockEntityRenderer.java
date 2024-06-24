9d8e3fa8ac36618df2a91191b8a6fa39847d4129
D4RKAR117 <D4RKAR117@outlook.es>
2024-06-23 21:04:39 -0500

    fix(shaft): fixed vertical position rendering

--- src/main/java/dev/darkar/cog_works/block/entity/renderer/TransmissionShaftBlockEntityRenderer.java
+++ src/main/java/dev/darkar/cog_works/block/entity/renderer/TransmissionShaftBlockEntityRenderer.java
@@ -1,6 +1,7 @@
 package dev.darkar.cog_works.block.entity.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import dev.darkar.cog_works.block.entity.TransmissionShaftBlockEntity;
 import dev.darkar.cog_works.block.entity.model.TransmissionShaftBlockEntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -26,6 +27,19 @@ public class TransmissionShaftBlockEntityRenderer extends GeoBlockRenderer<Trans
 	
 	@Override
 	protected void rotateBlock(Direction facing, PoseStack poseStack) {
-		super.rotateBlock(facing, poseStack);
+		switch (facing) {
+			case SOUTH -> poseStack.mulPose(Axis.YP.rotationDegrees(180));
+			case WEST -> poseStack.mulPose(Axis.YP.rotationDegrees(90));
+			case NORTH -> poseStack.mulPose(Axis.YP.rotationDegrees(0));
+			case EAST -> poseStack.mulPose(Axis.YP.rotationDegrees(270));
+			case UP -> {
+				poseStack.mulPose(Axis.XP.rotationDegrees(90));
+				poseStack.translate(0, -0.5D, -0.5D);
+			}
+			case DOWN -> {
+				poseStack.mulPose(Axis.XN.rotationDegrees(90));
+				poseStack.translate(0, -0.5D, 0.5D);
+			}
+		}
 	}
 }
